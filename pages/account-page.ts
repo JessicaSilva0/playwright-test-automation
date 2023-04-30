802b2735c78378b2a4890b481ad209d1a57e8d03
jessica-silva <jessica.silva@soluttions.com.br>
2023-04-30 01:14:12 +0100

    test data: Add account number, account-page.ts and account-test.spec.ts changed accountNumber

--- pages/account-page.ts
+++ pages/account-page.ts
@@ -1,21 +1,21 @@
 import { expect, Page, test } from "@playwright/test";
 import * as step from '../steps/common-steps';
 import { faker } from '@faker-js/faker';
+import { accountNumber } from "../resource/test-data";
 
-type accountNumber = '800002' | '800003' | '4539082039396288';
 
 export async function chooseAccount(accountPage: Page, accountName: string){
     await test.step(`View Account Details and Select account: '${accountName}'` , async () => {
         const listAccount = accountPage.locator('#listAccounts');
         switch (accountName){
             case "Savings":
-                await listAccount.selectOption('800002');
+                await listAccount.selectOption(accountNumber.Savings);
                 break;
             case "Checking":
-                await listAccount.selectOption('800003');
+                await listAccount.selectOption(accountNumber.Checking);
                 break;
             case "Credit Card":
-                await listAccount.selectOption('4539082039396288');
+                await listAccount.selectOption(accountNumber.CreditCard);
                 break;
             case undefined: console.log("This account doesn't exist, please check again");
                 break
@@ -31,21 +31,21 @@ export async function goToTransferFunds(accountPage: Page){
     });
 }
 
-export async function selectTransferFromAccount(accountPage: Page, accountNumber: accountNumber){
+export async function selectTransferFromAccount(accountPage: Page, accountNumber: string){
     await test.step(`Transfer From account: ${accountNumber}`, async () => {
         await accountPage.locator('#fromAccount').selectOption(accountNumber);
     });
 }
 
-export async function selectTransferToAccount(accountPage: Page, accountNumber: accountNumber){
+export async function selectTransferToAccount(accountPage: Page, accountNumber: string){
     await test.step(`Transfer To account: ${accountNumber}`, async () => {
         await accountPage.locator('#toAccount').selectOption(accountNumber);
     });
 }
 
 export async function typeAmountToTransfer(accountPage: Page){
-    await test.step(`Amount to Transfer`, async () => {
-        const amount = faker.finance.amount()
+    const amount = faker.finance.amount()
+    await test.step(`enter the amount to be transferred: '${amount}'`, async () => {
         await accountPage.locator('#transferAmount').type(amount);
     });
 }
@@ -55,10 +55,10 @@ export async function clickTransferMoney(accountPage: Page){
     await expect(accountPage.getByText("was successfully transferred from Account")).toBeVisible();
 }
 
-export async function transferFundsFromOneAccountToAnother(accountPage: Page, accountNumber: accountNumber){
+export async function transferFundsFromOneAccountToAnother(accountPage: Page, accountFrom: string, accountTarget: string){
     await goToTransferFunds(accountPage);
-    await selectTransferFromAccount(accountPage, accountNumber);
-    await selectTransferToAccount(accountPage, accountNumber);
+    await selectTransferFromAccount(accountPage, accountFrom);
+    await selectTransferToAccount(accountPage, accountTarget);
     await typeAmountToTransfer(accountPage);
     await clickTransferMoney(accountPage);
 }
--- tests/account-test.spec.ts
+++ tests/account-test.spec.ts
@@ -1,24 +1,16 @@
 import { test, expect } from '@playwright/test';
 import * as loginPage from '../pages/login-page';
 import * as accountPage from '../pages/account-page';
-import { user } from "../resource/test-data";
+import { user, accountNumber } from "../resource/test-data";
 
-test.only("user should be able to use your Online Banking Account", async ({page: page}) => {
+test.only("User should be able to transfer money in your Online Banking Account", async ({page: page}) => {
     await test.step("Sign In with user credentials", async () => {
         await loginPage.signInAccount(page, user.username, user.password);
         await expect(page.getByText('Welcome to Altoro Mutual Online.')).toBeVisible();
     });
-/*    await test.step("Select Account Details", async () => {
-        await accountPage.chooseAccount(page, "Checking");
-        await accountPage.goToTransferFunds(page);
-        await accountPage.selectTransferFromAccount(page, "800003");
-        await accountPage.selectTransferToAccount(page, "800002");
-        await accountPage.typeAmountToTransfer(page);
-    });
 
- */
     await test.step("Transfer some Money to another account", async () => {
-        await accountPage.transferFundsFromOneAccountToAnother(page, "800003");
+        await accountPage.transferFundsFromOneAccountToAnother(page, accountNumber.Checking, accountNumber.Savings);
     });
 
 });
